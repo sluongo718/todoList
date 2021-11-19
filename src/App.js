d372d2a4268a57b36539e785a30671b7a7b94b69
sluongo718 <sluongo718@yahoo.com>
2021-11-19 18:38:11 -0500

    created stor and reducer

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
             />
             <input type="submit"/>
         </form>
+
   
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
+import taskReducer from './reducers/taskReducer';
 
+const store = createStore(taskReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
